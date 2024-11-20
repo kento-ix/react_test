c62e8bac20cf92b3101dbacfa3a71088a7fc2fb7
kento-ix <kentoix1105@gmail.com>
2024-11-20 15:09:29 -0800

    added react router dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,19 @@
 import React from 'react';
 import './App.css';
+import { Route, Routes } from 'react-router-dom';
+import Home from './pages/home';
+import Myinfo from './pages/myinfo';
+import NavBar from './component/navbar';
 
 function App() {
   return (
-    <div>
-
-    </div>
+    <>
+      <NavBar/>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/myinfo" element={<Myinfo />} />
+      </Routes>
+    </>
   );
 }
 
